37d00df1da54e23747f5c67b1ee84fab6731bde5
Diego <diego31199@gmail.com>
2021-04-20 11:55:36 -0400

    siiu

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,10 @@ app.get('/', (req, res) => {
 });
 */
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+7*app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+);*/
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
