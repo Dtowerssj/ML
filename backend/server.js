25d54fb941da7030111bc87a5e71c8099e15f495
Diego <diego31199@gmail.com>
2021-04-22 02:12:13 -0400

    hola

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
@@ -37,13 +38,13 @@ app.get('/', (req, res) => {
 });
 */
 
-/*app.use(express.static(path.join(__dirname, '/frontend/build')));
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);*/
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
+);
+
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
